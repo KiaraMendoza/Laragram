f6d10a78e0f3e09c36b1a1ca5cb79c51bf72f55f
Light <kiaraymg@gmail.com>
2020-07-16 14:25:57 +0200

    Styled homepage to be similar as instagram, and added a username on User's registration (on the registration form, on the RegisterController, on the migration and on the User Schema).

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+<div class="container-lg px-4 px-lg-0">
+    <div class="row justify-content-center pt-5">
+        <div class="col-4 d-flex justify-content-center align-items-center">
+            <img class="user-profile-image rounded-circle" src="https://placedog.net/550/550" alt="user profile">
+        </div>
+        <div class="col-8">
+            <div><h1 class="user-profile-username">Doggy remaker</h1></div>
+            <div class="user-profile-numbers d-flex">
+                <div class="pr-4">
+                    <p><strong class="profile-numbers pr-2">302</strong>Publications</p>
+                </div>
+                <div class="pr-4">
+                    <p><strong class="profile-numbers pr-2">122</strong>Followers</p>
+                </div>
+                <div class="pr-4">
+                    <p><strong class="profile-numbers pr-2">12</strong>Following</p>
                 </div>
             </div>
+            <div class="user-profile-website pt-4"><p class="mb-0 font-weight-bold">doggy-remaker.guau</p></div>
+            <div class="user-profile-info"><p class="mb-0">We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</p></div>
+            <div class="user-profile-url"><a href="#">doggy-remaker.guau</a></div>
         </div>
+    </div>
+    <div class="grid pt-5">
+        <div class="grid-item"><img src="https://placedog.net/550/550" alt="#"></div>
+        <div class="grid-item"><img src="https://placedog.net/550/550" alt="#"></div>
+        <div class="grid-item"><img src="https://placedog.net/550/550" alt="#"></div>
     </div>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,8 +23,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
+                    <div><img class="pr-3" src="/svg/laragramLogo.svg" alt="website logo" style="max-height:2rem; border-right: 1px solid #333"></div>
+                    <div class="pl-3">Laragram</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -51,7 +52,7 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    {{ Auth::user()->username }} <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
